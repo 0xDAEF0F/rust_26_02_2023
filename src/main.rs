e17d5406a9ec62344118aea0a33152ee6146aa98
daemon <0xDAEF0F@proton.me>
2023-02-26 18:18:57 -0700

    4.2 References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-// 4 Understanding Ownership
-// 4.1 What is Ownership?
+// 4.2 References and Borrowing
 fn main() {
     // example_0();
     // example_1();
@@ -7,115 +6,141 @@ fn main() {
     // example_3();
     // example_4();
     // example_5();
-    example_6();
+    // example_6();
+    example_7();
 }
 
 #[allow(dead_code)]
-#[allow(unused_variables)]
 fn example_0() {
-    let s = "hello";
-    {
-        // another scope
-        let s = "another hello";
-    }
-    // This String type is allocated on the heap
-    // unknown at compile time and growable
-    {
-        let mut s2 = String::from("hello");
-        s2.push_str(", world!");
-        // Once the variable goes out of scope, the memory is freed
-        println!("{}", s2);
-    }
+    let s1 = String::from("hello, ser");
+    // this pattern is called borrowing
+    let len = calculate_length(&s1);
+    // we can pass a reference to calculate_length
+    // and s1 will still be valid and owned by this scope
+    println!("The length of '{}' is {}.", s1, len);
 }
+
+fn calculate_length(s: &String) -> usize {
+    // a reference allows us to use a value without taking ownership
+    s.len()
+
+    // NOTE: s.push_str(", world"); // this will not work because we do not have ownership
+} // s goes out of scope, but since it does not have ownership of what it refers to, it is not dropped.
 
 #[allow(dead_code)]
 fn example_1() {
-    let s1 = String::from("hello");
-    let s2 = s1;
-    // when we assign s1 to s2, the String data is copied, meaning
-    // we copy the pointer, the length, and the capacity that are
-    // on the stack
+    // declare a variable that is mutable
+    let mut s = String::from("hello ser");
+    change(&mut s); // create mutable reference
 
-    // This will error because s1 is no longer valid
-    // println!("{}, world!", s1);
+    println!("The string is now: {}", s);
+}
 
-    // OK!
-    println!("{}, world!", s2);
+// This &mut makes very clear that the value will mutate
+fn change(some_string: &mut String) {
+    some_string.push_str(", another string");
 }
 
 #[allow(dead_code)]
 fn example_2() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    // This will work because we are explicitly calling clone
-    // which will copy the heap data
-    println!("s1 = {}, s2 = {}", s1, s2);
+    // If you have a mutable reference to a value,
+    // you can have no other references to that value.
+    let mut s = String::from("mutable string");
+
+    // Fine!
+    let r1 = &mut s;
+    r1.push('.');
+
+    // Not Fine!
+    // let r2 = &mut s;
+
+    println!("r1 is {}", r1);
 }
 
 #[allow(dead_code)]
 fn example_3() {
-    // This is fine!
-    let x = 5;
-    let y = x;
+    let mut s = String::from("mutable string");
 
-    // types such as integers that have a known
-    // size at compile time are stored entirely
-    // on the stack.
-    println!("x = {}, y = {}", x, y);
+    {
+        let r1 = &mut s;
+        r1.push('.');
+    } // r1 gets dropped here
+
+    #[allow(unused_variables)]
+    let r3 = &s; // no problem
+    let r2 = &mut s; // no problem
+
+    // BIG PROBLEM
+    // let r3 = &mut s;
+
+    println!("r2 is {r2}");
 }
 
 #[allow(dead_code)]
+#[allow(unused_mut)]
 fn example_4() {
-    let s = String::from("hello");
-    takes_ownership(s);
-    // This will error because s is no longer valid
-    // println!("{}", s);
+    // You can have multiple immutable references to a value
+    // but you can only have one mutable reference to a value
+    let mut s = String::from("mutable string");
 
-    let x = 5;
-    makes_copy(x);
-    // This will work because x is an integer
-    // which is stored on the stack
-    println!("{}", x);
-}
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
-}
+    // BIG PROBLEM
+    // let r3 = &mut s;
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+    println!("{}, {}", r1, r2);
+    // println!("{}, {}, and {}", r1, r2, r3);
 }
 
 #[allow(dead_code)]
 fn example_5() {
-    let s1 = gives_ownership();
-    println!("{}", s1);
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    // let mut s = String::from("mutable string");
 
-    // This will error because s2 is no longer valid
-    // println!("{}", s2);
+    // let r1 = &s;
+    // let r2 = &mut s;
 
-    println!("{}", s3);
-}
-
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-    return some_string;
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    return a_string;
+    // ERROR
+    // println!("{}, {}", r1, r2);
 }
 
 #[allow(dead_code)]
 fn example_6() {
-    let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is {}.", s2, len);
+    let mut s = String::from("hello");
+    println!("s is {}", s);
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+
+    s.push_str(", world!");
+
+    let r3 = &mut s;
+
+    r3.push('!');
+
+    println!("{}", r3);
+    println!("{}", s);
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    return (s, length);
+#[allow(dead_code)]
+fn example_7() {
+    // let reference_to_nothing = dangle();
+    let h = no_dangle();
+    println!("h is {}", h);
 }
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+
+//     &s
+// }
+
+fn no_dangle() -> String {
+    return String::from("hello");
+}
+
+// Rules of References
+// 1. At any given time, you can have either one mutable reference or any number of immutable references.
+// 2. References must always be valid.

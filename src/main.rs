18de4844095f0af504749391babff8f1ae16806a
daemon <0xDAEF0F@proton.me>
2023-02-27 09:31:29 -0700

    4.3 The Slice Type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// 4.2 References and Borrowing
+// 4.3 The Slice Type
 fn main() {
     // example_0();
     // example_1();
@@ -12,135 +12,164 @@ fn main() {
 
 #[allow(dead_code)]
 fn example_0() {
-    let s1 = String::from("hello, ser");
-    // this pattern is called borrowing
-    let len = calculate_length(&s1);
-    // we can pass a reference to calculate_length
-    // and s1 will still be valid and owned by this scope
-    println!("The length of '{}' is {}.", s1, len);
+    let mut s = String::from("hello world");
+
+    let fw = first_word(&s); // 5
+
+    s.clear(); // empties s: ""
+
+    // fw is still 5, but value of s is now ""
+    println!("first word ended at index: {fw}");
+    println!("string is now: \"{s}\"");
 }
 
-fn calculate_length(s: &String) -> usize {
-    // a reference allows us to use a value without taking ownership
-    s.len()
+// b' ' is a byte literal
+// bytes.iter() returns an iterator over the bytes of the string
+// enumerate() wraps the result of bytes.iter() and returns each element as part of a tuple
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
 
-    // NOTE: s.push_str(", world"); // this will not work because we do not have ownership
-} // s goes out of scope, but since it does not have ownership of what it refers to, it is not dropped.
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    return s.len();
+}
 
 #[allow(dead_code)]
 fn example_1() {
-    // declare a variable that is mutable
-    let mut s = String::from("hello ser");
-    change(&mut s); // create mutable reference
+    // enter: String Slices
+    let mut s = String::from("hello world");
 
-    println!("The string is now: {}", s);
-}
+    let hello = &s[0..5];
+    let world = &s[6..11];
 
-// This &mut makes very clear that the value will mutate
-fn change(some_string: &mut String) {
-    some_string.push_str(", another string");
+    println!("the first word is: \"{hello}\"");
+    println!("the second word is: \"{world}\"");
+
+    s.clear();
+
+    // this will panick because index 5 does not exist
+    let not_hello = &s[0..5];
+
+    println!("not_hello: {not_hello}");
 }
 
 #[allow(dead_code)]
 fn example_2() {
-    // If you have a mutable reference to a value,
-    // you can have no other references to that value.
-    let mut s = String::from("mutable string");
+    let s = String::from("hello");
 
-    // Fine!
-    let r1 = &mut s;
-    r1.push('.');
+    // Equivalent
+    let he = &s[0..2];
+    let he2 = &s[..2];
+    assert_eq!(he, he2);
 
-    // Not Fine!
-    // let r2 = &mut s;
+    // Equivalent
+    let len = s.len();
+    let lo = &s[3..len];
+    let lo2 = &s[3..];
+    assert_eq!(lo, lo2);
 
-    println!("r1 is {}", r1);
+    // Equivalent
+    let all = &s[0..len];
+    let all2 = &s[..];
+    assert_eq!(all, all2);
+
+    let mut s2 = String::from("hello");
+    println!("s2 before: \"{s2}\"");
+    s2.clear();
+    println!("s2 after: \"{s2}\"");
 }
 
 #[allow(dead_code)]
 fn example_3() {
-    let mut s = String::from("mutable string");
+    let s = String::from("hello ser");
+    let fw = fw(&s);
+    println!("fw is: {fw}");
 
-    {
-        let r1 = &mut s;
-        r1.push('.');
-    } // r1 gets dropped here
+    let sw = second_word(&s);
+    println!("sw is: {sw}");
+}
 
-    #[allow(unused_variables)]
-    let r3 = &s; // no problem
-    let r2 = &mut s; // no problem
+fn fw(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-    // BIG PROBLEM
-    // let r3 = &mut s;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
 
-    println!("r2 is {r2}");
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[i + 1..];
+        }
+    }
+
+    &s[..]
 }
 
 #[allow(dead_code)]
 #[allow(unused_mut)]
 fn example_4() {
-    // You can have multiple immutable references to a value
-    // but you can only have one mutable reference to a value
-    let mut s = String::from("mutable string");
+    let mut s = String::from("hello world");
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
+    let fw = fw(&s);
 
-    // BIG PROBLEM
-    // let r3 = &mut s;
+    // If uncommented
+    // Will error out: cannot borrow `s` as mutable because it is also borrowed as immutable
+    // when trying to print fw
+    // s.clear();
 
-    println!("{}, {}", r1, r2);
-    // println!("{}, {}, and {}", r1, r2, r3);
+    println!("the first word is: {fw}");
+    println!("the string is: {s}");
 }
 
 #[allow(dead_code)]
 fn example_5() {
-    // let mut s = String::from("mutable string");
-
-    // let r1 = &s;
-    // let r2 = &mut s;
-
-    // ERROR
-    // println!("{}, {}", r1, r2);
+    let s = "Hello, world!";
+    let first_word = fw(&s);
+    assert_eq!(first_word, "Hello,");
+    println!("first word: {first_word}");
+    println!("entire string: {s}");
 }
 
 #[allow(dead_code)]
+#[allow(unused_variables)]
 fn example_6() {
-    let mut s = String::from("hello");
-    println!("s is {}", s);
+    let my_string = String::from("hello world");
 
-    let r1 = &s;
-    let r2 = &s;
-    println!("{} and {}", r1, r2);
-    // r1 and r2 are no longer used after this point
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = fw(&my_string[0..6]);
+    let word = fw(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = fw(&my_string);
 
-    s.push_str(", world!");
+    let my_string_literal = "hello world";
 
-    let r3 = &mut s;
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = fw(&my_string_literal[0..6]);
+    let word = fw(&my_string_literal[..]);
 
-    r3.push('!');
-
-    println!("{}", r3);
-    println!("{}", s);
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = fw(my_string_literal);
 }
 
 #[allow(dead_code)]
 fn example_7() {
-    // let reference_to_nothing = dangle();
-    let h = no_dangle();
-    println!("h is {}", h);
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
-
-// fn dangle() -> &String {
-//     let s = String::from("hello");
-
-//     &s
-// }
-
-fn no_dangle() -> String {
-    return String::from("hello");
-}
-
-// Rules of References
-// 1. At any given time, you can have either one mutable reference or any number of immutable references.
-// 2. References must always be valid.

21fe3c454564a5b3158bb30600d3927d949c1d19
daemon <0xDAEF0F@proton.me>
2023-02-27 20:12:38 -0700

    5.1 defining and instantiating structs

--- src/main.rs
+++ src/main.rs
@@ -1,175 +1,121 @@
-// 4.3 The Slice Type
+// 5.1 Defining and Instantiating Structs
 fn main() {
     // example_0();
     // example_1();
     // example_2();
     // example_3();
-    // example_4();
-    // example_5();
-    // example_6();
-    example_7();
+    example_4();
 }
 
 #[allow(dead_code)]
-fn example_0() {
-    let mut s = String::from("hello world");
-
-    let fw = first_word(&s); // 5
-
-    s.clear(); // empties s: ""
-
-    // fw is still 5, but value of s is now ""
-    println!("first word ended at index: {fw}");
-    println!("string is now: \"{s}\"");
-}
-
-// b' ' is a byte literal
-// bytes.iter() returns an iterator over the bytes of the string
-// enumerate() wraps the result of bytes.iter() and returns each element as part of a tuple
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    return s.len();
-}
-
-#[allow(dead_code)]
-fn example_1() {
-    // enter: String Slices
-    let mut s = String::from("hello world");
-
-    let hello = &s[0..5];
-    let world = &s[6..11];
-
-    println!("the first word is: \"{hello}\"");
-    println!("the second word is: \"{world}\"");
-
-    s.clear();
-
-    // this will panick because index 5 does not exist
-    let not_hello = &s[0..5];
-
-    println!("not_hello: {not_hello}");
-}
-
-#[allow(dead_code)]
-fn example_2() {
-    let s = String::from("hello");
-
-    // Equivalent
-    let he = &s[0..2];
-    let he2 = &s[..2];
-    assert_eq!(he, he2);
-
-    // Equivalent
-    let len = s.len();
-    let lo = &s[3..len];
-    let lo2 = &s[3..];
-    assert_eq!(lo, lo2);
-
-    // Equivalent
-    let all = &s[0..len];
-    let all2 = &s[..];
-    assert_eq!(all, all2);
-
-    let mut s2 = String::from("hello");
-    println!("s2 before: \"{s2}\"");
-    s2.clear();
-    println!("s2 after: \"{s2}\"");
-}
-
-#[allow(dead_code)]
-fn example_3() {
-    let s = String::from("hello ser");
-    let fw = fw(&s);
-    println!("fw is: {fw}");
-
-    let sw = second_word(&s);
-    println!("sw is: {sw}");
-}
-
-fn fw(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-
-    &s[..]
-}
-
-fn second_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[i + 1..];
-        }
-    }
-
-    &s[..]
-}
-
-#[allow(dead_code)]
-#[allow(unused_mut)]
-fn example_4() {
-    let mut s = String::from("hello world");
-
-    let fw = fw(&s);
-
-    // If uncommented
-    // Will error out: cannot borrow `s` as mutable because it is also borrowed as immutable
-    // when trying to print fw
-    // s.clear();
-
-    println!("the first word is: {fw}");
-    println!("the string is: {s}");
-}
-
-#[allow(dead_code)]
-fn example_5() {
-    let s = "Hello, world!";
-    let first_word = fw(&s);
-    assert_eq!(first_word, "Hello,");
-    println!("first word: {first_word}");
-    println!("entire string: {s}");
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
-fn example_6() {
-    let my_string = String::from("hello world");
+fn example_0() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 
-    // `first_word` works on slices of `String`s, whether partial or whole
-    let word = fw(&my_string[0..6]);
-    let word = fw(&my_string[..]);
-    // `first_word` also works on references to `String`s, which are equivalent
-    // to whole slices of `String`s
-    let word = fw(&my_string);
+    // to read
+    let user1_email = user1.email;
+    println!("user1_email: {}", user1_email);
 
-    let my_string_literal = "hello world";
+    // to write
+    // entire instance *MUST* be mutable
+    user1.email = String::from("another_email@example.com");
+    println!("new user1_email: {}", user1.email);
 
-    // `first_word` works on slices of string literals, whether partial or whole
-    let word = fw(&my_string_literal[0..6]);
-    let word = fw(&my_string_literal[..]);
+    // create new user
+    let user2 = build_user(String::from("user2@xyz.xyz"), String::from("user2"));
+}
 
-    // Because string literals *are* string slices already,
-    // this works too, without the slice syntax!
-    let word = fw(my_string_literal);
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
 
 #[allow(dead_code)]
-fn example_7() {
-    let a = [1, 2, 3, 4, 5];
+#[allow(unused_variables)]
+fn example_1() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 
-    let slice = &a[1..3];
+    // valid to reference other struct
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@email.com"),
+        sign_in_count: user1.sign_in_count,
+    };
 
-    assert_eq!(slice, &[2, 3]);
+    // valid to destructure
+    // can no longer user user1
+    let user3 = User {
+        email: String::from("user3@email.com"),
+        ..user2
+    };
+
+    let user1_active = user1.active;
+    println!("is user1 active? {}", user1_active);
+
+    // can still reference user1 email. but not as a whole?
+    let user1_email = user1.email;
+    println!("user1_email: {}", user1_email);
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn example_2() {
+    // tuple structs
+    // they are different types, even if their fields
+    // are the same type
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+// unit-like struct
+struct AlwaysEqual;
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn example_3() {
+    let subject = AlwaysEqual;
+}
+
+#[allow(dead_code)]
+fn example_4() {
+    // error: missing lifetime specifier
+    // struct User {
+    //     active: bool,
+    //     username: &str,
+    //     email: &str,
+    //     sign_in_count: u64,
+    // }
+
+    // let user1 = User {
+    //     active: true,
+    //     username: "someusername123",
+    //     email: "someone@example.com",
+    //     sign_in_count: 1,
+    // };
 }

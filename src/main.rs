73b309a0531880fe992a8fccbee5c03b87d6028e
daemon <0xDAEF0F@proton.me>
2023-03-26 13:49:56 -0600

    26_03_2023

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,45 @@
-// 5.1 Defining and Instantiating Structs
+// 5.2 An Example Using Structs
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    // example_0();
-    // example_1();
-    // example_2();
-    // example_3();
-    example_4();
-}
+    let width1 = 30;
+    let height1 = 50;
+    println!(
+        "Method1: The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
 
-#[allow(dead_code)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+    let rect2 = (30, 50);
+    println!(
+        "Method2: The area of the rectangle is {} square pixels.",
+        area2(rect2)
+    );
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn example_0() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
+    let rect3 = Rectangle {
+        width: 30,
+        height: 50,
     };
-
-    // to read
-    let user1_email = user1.email;
-    println!("user1_email: {}", user1_email);
-
-    // to write
-    // entire instance *MUST* be mutable
-    user1.email = String::from("another_email@example.com");
-    println!("new user1_email: {}", user1.email);
-
-    // create new user
-    let user2 = build_user(String::from("user2@xyz.xyz"), String::from("user2"));
+    println!(
+        "Method3: The area of the rectangle is {} square pixels.",
+        area3(&rect3)
+    );
+    println!("the rectangle is {:?}", rect3);
+    // debugging
+    dbg!(&rect3);
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn example_1() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
-
-    // valid to reference other struct
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("another@email.com"),
-        sign_in_count: user1.sign_in_count,
-    };
-
-    // valid to destructure
-    // can no longer user user1
-    let user3 = User {
-        email: String::from("user3@email.com"),
-        ..user2
-    };
-
-    let user1_active = user1.active;
-    println!("is user1 active? {}", user1_active);
-
-    // can still reference user1 email. but not as a whole?
-    let user1_email = user1.email;
-    println!("user1_email: {}", user1_email);
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
-
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn example_2() {
-    // tuple structs
-    // they are different types, even if their fields
-    // are the same type
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
-}
-
-// unit-like struct
-struct AlwaysEqual;
-
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn example_3() {
-    let subject = AlwaysEqual;
-}
-
-#[allow(dead_code)]
-fn example_4() {
-    // error: missing lifetime specifier
-    // struct User {
-    //     active: bool,
-    //     username: &str,
-    //     email: &str,
-    //     sign_in_count: u64,
-    // }
-
-    // let user1 = User {
-    //     active: true,
-    //     username: "someusername123",
-    //     email: "someone@example.com",
-    //     sign_in_count: 1,
-    // };
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }

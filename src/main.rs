251b0f132913aff403902a890057ac987ad11e87
daemon <0xDAEF0F@proton.me>
2023-04-02 16:42:35 -0600

    02_04_2023

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,31 @@
-// 5.3 Defining an Enum
+// 6.2 The match control flow construct
 
-enum IpAddrKind {
-    V4,
-    V6,
-}
+// CHALLENGE
+// * construct a match statement
+// * extract value from the type of that match (try Option)
+fn main() {
+    let other_book = Book::Other;
+    let math_book_easy = Book::Math { iq: 100 };
+    let math_book_hard = Book::Math { iq: 150 };
 
-enum IpAddr2 {
-    V4(String),
-    V6(String),
-}
+    let book_arr = [&other_book, &math_book_easy, &math_book_hard];
 
-enum Message {
-    Quit,
-    // can have named fields
-    Move { x: i32, y: i32 },
-    Write(String),
-    // or tuple-like fields
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
+    let mut i = 0;
+    while i < 3 {
+        let is_hard = is_book_too_hard(&book_arr[i]);
+        println!("book {i} is: {is_hard}");
+        i += 1;
     }
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum Book {
+    Math { iq: u8 },
+    Other,
 }
 
-// very important concept!
-enum Option<T> {
-    None,
-    Some(T),
-}
-
-fn main() {
-    // instantiate enum
-    let _four = IpAddrKind::V4;
-    let _six = IpAddrKind::V6;
-
-    // instantiate struct
-    let _home: IpAddr = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    // instantiate struct two
-    let _loopback: IpAddr = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    // instantiate struct three
-    let _home2 = IpAddr2::V4(String::from("127.0.0.1"));
-    let _loopback2 = IpAddr2::V6(String::from("::1"));
-
-    let m = Message::Write(String::from("hello"));
-    m.call();
-
-    // option example
-    let some_number = Some(5);
-    let some_char = Some('a');
-
-    let absent_number: Option<i32> = Option::None;
-
-    let num = some_number.unwrap();
-    println!("num + 5: {}", num + 5);
+fn is_book_too_hard(book: &Book) -> bool {
+    match book {
+        Book::Math { iq } => iq > &120,
+        _ => false,
+    }
 }

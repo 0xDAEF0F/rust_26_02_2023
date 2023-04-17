4faf147b2ccc5e9e45fe966d8d5b410599a380ef
daemon <0xDAEF0F@proton.me>
2023-04-17 17:43:44 -0600

    17_04_2023

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-// Check other git branches for other examples
+// 9.1 Unrecoverable Error with `panic!`
+
+// Notes
+// * When reading from an out of bounds array, rust panics automatically.
+// * Whenever there is an unrecoverable error, there is no choice but to panic.
 fn main() {
-    println!("Hello, world!");
+    // automatic panicking
+    let v = vec![1, 2, 3];
+
+    v[99];
+
+    // manual panicking
+    panic!("crash and burn!");
 }
